2fecc3e915abbced8dd74d77b87cf234c6f13e1e
pragatibhoyte <pragatibhoyte73@gmail.com>
2026-06-26 14:23:16 +0530

    Programs to accept and display details of user using variations

---
+++ Application Programs/program22.py
@@ -0,0 +1,12 @@
+def main():
+
+    sName = input("Enter your Name : ")
+    iAge = int(input("Enter your Age : "))
+    fMarks = float(input("Enter your Marks : "))
+
+    print(f"Your Name is : {sName}")
+    print(f"Your Age is : {iAge}")
+    print(f"Your Marks are : {fMarks}")
+
+if __name__ == "__main__":
+    main()
